45836217b2a20dee98f00e14721694fc594cbc69
Tom Elliott <tom.w.elliott@gmail.com>
2018-06-24 12:51:45 -0400

    Fix note letters in scale

--- examples/scale/main.go
+++ examples/scale/main.go
@@ -14,22 +14,22 @@ func main() {
 	defer player.Close()
 
 	var (
-		c  = wavegenerator.SinTone(220.0)
-		d  = c * (1 + 1.0/8.0)
-		e  = c * (1 + 2.0/8.0)
-		f  = c * (1 + 2.5/8.0)
-		g  = c * (1 + 4.0/8.0)
-		a  = c * (1 + 5.5/8.0)
-		b  = c * (1 + 7/8.0)
-		c2 = c * 2
+		a      = wavegenerator.TriangleTone(220.0)
+		b      = a * (1 + 1.0/8.0)
+		c      = a * (1 + 2.0/8.0)
+		cSharp = a * (1 + 2.5/8.0)
+		e      = a * (1 + 4.0/8.0)
+		fSharp = a * (1 + 5.5/8.0)
+		g      = a * (1 + 7/8.0)
+		A      = a * 2
 	)
 
-	player.Play(time.Second/4, c)
-	player.Play(time.Second/4, d)
-	player.Play(time.Second/4, e)
-	player.Play(time.Second/4, f)
-	player.Play(time.Second/4, g)
 	player.Play(time.Second/4, a)
 	player.Play(time.Second/4, b)
-	player.Play(time.Second/4, c2)
+	player.Play(time.Second/4, c)
+	player.Play(time.Second/4, cSharp)
+	player.Play(time.Second/4, e)
+	player.Play(time.Second/4, fSharp)
+	player.Play(time.Second/4, g)
+	player.Play(time.Second/4, A)
 }
