16ecc10224b76ad37f89a8aaad04b0ac903bb93b
Tom Elliott <tom.w.elliott@gmail.com>
2018-06-25 23:52:53 -0400

    Remove unused tone implementations

--- tones.go
+++ tones.go
@@ -7,33 +7,3 @@ type Tone interface {
 	Frequency() float64
 	Voice() Voice
 }
-
-type TriangleTone float64
-
-func (t TriangleTone) Frequency() float64 {
-	return float64(t)
-}
-
-func (t TriangleTone) Voice() Voice {
-	return Triangle
-}
-
-type SquareTone float64
-
-func (t SquareTone) Frequency() float64 {
-	return float64(t)
-}
-
-func (t SquareTone) Voice() Voice {
-	return Square
-}
-
-type SinTone float64
-
-func (t SinTone) Frequency() float64 {
-	return float64(t)
-}
-
-func (t SinTone) Voice() Voice {
-	return Sin
-}
