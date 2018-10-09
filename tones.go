da73dd5d4637953620f6a0bf66df9111fae65260
Tom Elliott <tom.w.elliott@gmail.com>
2018-10-08 23:40:53 -0400

    Support simple tones without an instrument.

--- tones.go
+++ tones.go
@@ -7,3 +7,30 @@ type Tone interface {
 	Frequency() float64
 	Voice() Voice
 }
+
+// NewTone returns a tone with the specified properties
+func NewTone(duration time.Duration, frequency float64, voice Voice) Tone {
+	return &simpleTone{
+		duration:  duration,
+		frequency: frequency,
+		voice:     voice,
+	}
+}
+
+type simpleTone struct {
+	duration  time.Duration
+	frequency float64
+	voice     Voice
+}
+
+func (s *simpleTone) Duration() time.Duration {
+	return s.duration
+}
+
+func (s *simpleTone) Frequency() float64 {
+	return s.frequency
+}
+
+func (s *simpleTone) Voice() Voice {
+	return s.voice
+}
